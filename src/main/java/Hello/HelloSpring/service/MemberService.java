8b46e65b3d400b59347806d82108d98570756f7e
root <ring3367@naver.com>
2022-12-20 15:15:07 +0900

    Component Scan

--- src/main/java/Hello/HelloSpring/service/MemberService.java
+++ src/main/java/Hello/HelloSpring/service/MemberService.java
@@ -3,14 +3,18 @@ package Hello.HelloSpring.service;
 import Hello.HelloSpring.domain.Member;
 import Hello.HelloSpring.repository.MemberRepository;
 import Hello.HelloSpring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
